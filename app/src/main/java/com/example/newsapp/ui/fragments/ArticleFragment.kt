085727b76263cdb4135306ae2f2336115d4d4eb1
Talgat Mussa <msstalgat01@gmail.com>
2023-02-09 17:13:31 +0500

    Implemented functionality for opening news in WebView.
    * Fixed  java.lang.NullPointerException: 'Attempt to invoke virtual method 'int java.lang.String.hashCode()'

--- app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
 import com.example.newsapp.databinding.FragmentArticleBinding
 import com.example.newsapp.db.ArticleDatabase
 import com.example.newsapp.repository.NewsRepository
 import com.example.newsapp.ui.NewsViewModel
 import com.example.newsapp.ui.NewsViewModelProviderFactory
+import com.google.android.material.snackbar.Snackbar
 
 class ArticleFragment: Fragment() {
     private var _binding: FragmentArticleBinding? = null
     private val binding get() = _binding!!
+    private val args: ArticleFragmentArgs by navArgs()
 
     val viewModel: NewsViewModel by activityViewModels() {
         NewsViewModelProviderFactory(NewsRepository(ArticleDatabase.getDatabase(requireContext())))
@@ -27,6 +31,15 @@ class ArticleFragment: Fragment() {
     ): View? {
         _binding = FragmentArticleBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val article = args.article
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/newsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/SavedNewsFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newsapp.R
+import com.example.newsapp.adapters.NewsAdapter
 import com.example.newsapp.databinding.FragmentSavedNewsBinding
 import com.example.newsapp.db.ArticleDatabase
+import com.example.newsapp.models.Article
 import com.example.newsapp.repository.NewsRepository
 import com.example.newsapp.ui.NewsViewModel
 import com.example.newsapp.ui.NewsViewModelProviderFactory
@@ -15,6 +21,7 @@ import com.example.newsapp.ui.NewsViewModelProviderFactory
 class SavedNewsFragment: Fragment() {
     private var _binding: FragmentSavedNewsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var newsAdapter: NewsAdapter
 
     val viewModel: NewsViewModel by activityViewModels() {
         NewsViewModelProviderFactory(NewsRepository(ArticleDatabase.getDatabase(requireContext())))
@@ -29,8 +36,28 @@ class SavedNewsFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter {article -> onItemClick(article)}
+        binding.rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun onItemClick(article: Article) {
+        findNavController().navigate(
+            R.id.action_savedNewsFragment_to_articleFragment,
+            bundleOf("article" to article)
+        )
     }
 }
